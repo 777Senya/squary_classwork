5f36a5cd74b6d6c685d8d4490820f89730794792
Юра Сенчина <senchina.u@gmail.com>
2016-11-10 17:46:58 +0000

    new

---
+++ index.js
@@ -0,0 +1,21 @@
+/*global $*/
+/*
+$(function(){
+    $('h1').html('Привіт ДЖейКВЕРІ'); сначала грузит ШТМЛ а потом додает скрипт
+})
+*/
+
+
+/*global $*/
+/*
+$(function(){
+    $('button').click(function(){
+        $('h1').toggleClass('marked');
+    })
+})
+*/
+$(function(){
+    $('button').click(function(){
+        $('h1').toggle(5000);
+    })
+})
